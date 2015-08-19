9e46cfc1e48e76913cb1e67c6c6b9fde52e248e9
Akshay S Dinesh <asdofindia@gmail.com>
2015-08-19 18:50:02 +0530

    switch to html4 mode

--- controllers/router.js
+++ controllers/router.js
@@ -3,7 +3,6 @@ angular.module("vibe")
 .config(function($stateProvider, $urlRouterProvider, $locationProvider) {
 
 	$urlRouterProvider.otherwise("/events");
-	$locationProvider.html5Mode(true);
 
 	$stateProvider
 	.state("events", {
